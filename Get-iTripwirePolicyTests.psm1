7d38d8781d63303f9078fd12c994b15a4fdf6c0c
davidjblackburn <40555710+davidjblackburn@users.noreply.github.com>
2019-12-28 21:14:37 -0800

    Update Get-iTripwirePolicyTests.psm1

--- Get-iTripwirePolicyTests.psm1
+++ Get-iTripwirePolicyTests.psm1
@@ -1,54 +1,12 @@
 ﻿Function Get-iTripwirePolicyTests
 {
 param (
-    [string]$server          = "tripwire-prod.oa.caiso.com",
+    [string]$systemname      = "tripwire-prod.company.com",      # Use the Tripwire server name.
     [string]$policyId        = "",
             $websession,
     [int]   $pagelimit       = 100,
-    [int]   $pagestart       = 0,
-    [string]$logserver       = "tripwire-prod.oa.caiso.com",
-    [string]$logdatabase     = "infosecrisks_prod",
-    [string]$logtable        = "infosecrisksLog",
-    [switch]$logtoout        = $false,
-    [switch]$logtoserver     = $false,
-    [int]$severity           = 6
-
+    [int]   $pagestart       = 0
 )
-
-$start_time                  = Get-Date
-
-$syslog_Array                = Set-SyslogArr
-
-$syslog_Array.facility       = 22
-$syslog_Array.severity       = $severity
-$syslog_Array.version        = 1
-$syslog_Array.hostname       = ([System.Net.DNS]::GetHostByName('').HostName).ToLower()
-$syslog_Array.appname        = ($MyInvocation.MyCommand).Name
-$syslog_Array.procid         = "-"
-$syslog_Array.msgid          = "calc"
-$syslog_Array.structureddata = "-"
-$syslog_Array.logdatabase    = $logdatabase
-$syslog_Array.logserver      = $logserver
-$syslog_Array.logtable       = $logtable
-$syslog_Array.logtoout       = $logtoout
-$syslog_Array.logtoserver    = $logtoserver
-
-
-if ( $severity -lt 0 -or $severity -gt 7 )
-{
-    $syslog_Array.msg        = "-LogLevel must be a number in the range [0..7]. Quitting collection."
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-
-    Write-SyslogArr $syslog_Array
-    return
-}
-
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "Start script"
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-SyslogArr $syslog_Array
-}
 
 add-type @"
     using System.Net;
@@ -67,15 +25,9 @@ add-type @"
 $headerplaintext = @{"Accept"="text/plain"}
 $headerappjson   = @{"Accept"="application/json"}
 
-$uri           = "https://$server/api/v1/policytests?policyId=$policyId&pageLimit=$pagelimit&pageStart=$pagestart"
+$uri           = "https://$systemname/api/v1/policytests?policyId=$policyId&pageLimit=$pagelimit&pageStart=$pagestart"
 
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "Get list of Tripwire policies"
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-SyslogArr $syslog_Array
-}
-
+# Get list of Tripwire policies
 $results       = Invoke-RestMethod -uri $uri -Method get -WebSession $websession
 $policytests   = $results
 $pagestart_t   = $pagestart
@@ -85,19 +37,11 @@ if ($results.count -eq $pagelimit)
     do
     {
             $pagestart_t       += $pagelimit
-            $uri                = "https://$server/api/v1/policytests?policyId=$policyId&pageLimit=$pagelimit&pageStart=$pagestart_t"
+            $uri                = "https://$systemname/api/v1/policytests?policyId=$policyId&pageLimit=$pagelimit&pageStart=$pagestart_t"
             $results            = Invoke-RestMethod -uri $uri -Method get -WebSession $websession
             $policytests       += $results
 
     } while ($results.count -eq $pagelimit)
-}
-
-$timespan                    = (New-TimeSpan -Start $start_time -End (Get-Date)).TotalMinutes
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "End script after $timespan minutes"
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-SyslogArr $syslog_Array
 }
 
 $policytests
