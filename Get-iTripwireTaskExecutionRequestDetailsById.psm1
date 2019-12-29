57935db567803132578fff47c39dbd46d0e7b158
davidjblackburn <40555710+davidjblackburn@users.noreply.github.com>
2019-12-28 21:14:51 -0800

    Update Get-iTripwireTaskExecutionRequestDetailsById.psm1

--- Get-iTripwireTaskExecutionRequestDetailsById.psm1
+++ Get-iTripwireTaskExecutionRequestDetailsById.psm1
@@ -2,40 +2,9 @@
 {
 param (
     $websession,
-    [string]$systemname  = "tripwire-prod.oa.caiso.com",
-    [string]$logserver   = "tripwire-prod.oa.caiso.com",
-    [string]$requestId   = "1360302",
-    [string]$logdatabase = "infosecrisks_prod",
-    [string]$logtable    = "InfoSecRisksLog",
-    [switch]$logtoout    = $true,
-    [switch]$logtoserver = $false,
-    [int]$severity       = 6
+    [string]$systemname      = "tripwire-prod.company.com",      # Use the Tripwire server name.
+    [string]$requestId       = "1360302"                         #   
     )
-$syslog_Array                = Set-SyslogArr
-
-$syslog_Array.facility       = 22
-$syslog_Array.severity       = $severity
-$syslog_Array.version        = 1
-$syslog_Array.hostname       = ([System.Net.DNS]::GetHostByName('').HostName).ToLower()
-$syslog_Array.appname        = ($MyInvocation.MyCommand).Name
-$syslog_Array.procid         = "-"
-$syslog_Array.msgid          = "calc"
-$syslog_Array.structureddata = "-"
-$syslog_Array.logdatabase    = $logdatabase
-$syslog_Array.logserver      = $logserver
-$syslog_Array.logtable       = $logtable
-$syslog_Array.logtoout       = $logtoout
-$syslog_Array.logtoserver    = $logtoserver
-
-
-if ( $severity -lt 0 -or $severity -gt 7 )
-{
-    $syslog_Array.msg        = "-LogLevel must be a number in the range [0..7]. Quitting collection."
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-
-    Write-SyslogArr $syslog_Array
-    return
-}
 
 add-type @"
     using System.Net;
