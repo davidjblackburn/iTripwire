b25a0f950485776760880086cfdf705192a610ce
davidjblackburn <40555710+davidjblackburn@users.noreply.github.com>
2019-12-28 18:43:56 -0800

    Create Get-iTripwireElementsList.psm1

---
+++ Get-iTripwireElementsList.psm1
@@ -0,0 +1,78 @@
+﻿Function Get-iTripwireElementsList
+{
+param (
+    $websession,
+    [string]$ruleid      = "-1y2p0ij32e7m9:-1y2p0ij30wmy3",
+    [string]$systemname  = "tripwire-prod.oa.caiso.com",
+    [string]$logserver   = "tripwire-prod.oa.caiso.com",
+    [int]$pagelimit      = 100,
+    [string]$logdatabase = "infosecrisks_prod",
+    [string]$logtable    = "InfoSecRisksLog",
+    [switch]$logtoout    = $true,
+    [switch]$logtoserver = $false,
+    [int]$severity       = 6
+    )
+$syslog_Array                = Set-SyslogArr
+
+$headerplaintext             = @{"Accept"="text/plain"}
+$headerappjson               = @{"Accept"="application/json"}
+
+$syslog_Array.facility       = 22
+$syslog_Array.severity       = $severity
+$syslog_Array.version        = 1
+$syslog_Array.hostname       = ([System.Net.DNS]::GetHostByName('').HostName).ToLower()
+$syslog_Array.appname        = ($MyInvocation.MyCommand).Name
+$syslog_Array.procid         = "-"
+$syslog_Array.msgid          = "calc"
+$syslog_Array.structureddata = "-"
+$syslog_Array.logdatabase    = $logdatabase
+$syslog_Array.logserver      = $logserver
+$syslog_Array.logtable       = $logtable
+$syslog_Array.logtoout       = $logtoout
+$syslog_Array.logtoserver    = $logtoserver
+
+
+if ( $severity -lt 0 -or $severity -gt 7 )
+{
+    $syslog_Array.msg        = "-LogLevel must be a number in the range [0..7]. Quitting collection."
+    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
+
+    Write-SyslogArr $syslog_Array
+    return
+}
+
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$jsonserial               = New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer
+$jsonserial.MaxJsonLength = [int]::MaxValue
+$elementslist             = $null
+
+$uri                      = "https://$systemname/api/v1/elements?ruleId=$ruleid"
+
+$elements                 = Invoke-RestMethod -uri $uri -Method get -WebSession $websession -Headers $headerappjson
+
+if (($elements.GetType()).BaseType.Name -ne "Array" )
+{
+    $elementslist     = $jsonserial.Deserialize($elements,[System.Object])
+}
+else
+{
+    $elementslist     = $elements
+}
+
+return $elementslist
+
+}
