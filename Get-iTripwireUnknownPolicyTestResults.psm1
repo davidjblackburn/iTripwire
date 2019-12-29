949465c865ce9e77e7cae96172e8d1da1577b4d5
davidjblackburn <40555710+davidjblackburn@users.noreply.github.com>
2019-12-28 21:15:01 -0800

    Update Get-iTripwireUnknownPolicyTestResults.psm1

--- Get-iTripwireUnknownPolicyTestResults.psm1
+++ Get-iTripwireUnknownPolicyTestResults.psm1
@@ -2,58 +2,13 @@
 {
 param (
             $websession,
-    [string]$policyId                 = "-1y2p0ij32e886:-1y2p0ij32bzxz",
-    #[string]$policyTestId             = "-1y2p0ij32e88h:-1y2p0ij32bzs0",
-    [string]$systemname               = "tripwire-prod.oa.caiso.com",
-    [string]$logserver                = "tripwire-prod.oa.caiso.com",
-    [string]$logdatabase              = "infosecrisks_prod",
-    [string]$logtable                 = "infosecrisksLog",
-    [switch]$logtoout                 = $false,
-    [switch]$logtoserver              = $false,
-    [int]$severity                    = 6
+    [string]$policyId        = "-1y2p0ij32e886:-1y2p0ij32bzxz",  # Use a policyId for testing.
+    [string]$systemname      = "tripwire-prod.company.com"      # Use the Tripwire server name.
     )
-$start_time                  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-$syslog_Array                = Set-SyslogArr
 
-$syslog_Array.facility       = 22
-$syslog_Array.severity       = $severity
-$syslog_Array.version        = 1
-$syslog_Array.hostname       = ([System.Net.DNS]::GetHostByName('').HostName).ToLower()
-$syslog_Array.appname        = ($MyInvocation.MyCommand).Name
-$syslog_Array.procid         = "-"
-$syslog_Array.msgid          = "calc"
-$syslog_Array.structureddata = "-"
-$syslog_Array.logdatabase    = $logdatabase
-$syslog_Array.logserver      = $logserver
-$syslog_Array.logtable       = $logtable
-$syslog_Array.logtoout       = $logtoout
-$syslog_Array.logtoserver    = $logtoserver
-
-if ( $severity -lt 0 -or $severity -gt 7 )
-{
-    $syslog_Array.msg        = "-LogLevel must be a number in the range [0..7]. Quitting collection."
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-
-    Write-SyslogArr $syslog_Array
-    return
-}
-
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "Start script"
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-SyslogArr $syslog_Array
-}
 $info                 = "" | Select-Object is_over_14_days_old, last_passed_days_old
 
-
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "Collect unknown policy test results"
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-SyslogArr $syslog_Array
-}
-
+# Collect unknown policy test results
 $uri                      = "https://$systemname/api/v1/policytestresults/unknownTestResults"
 $results                  = Invoke-RestMethod -uri $uri -Method get -WebSession $websession
 
@@ -75,14 +30,6 @@ foreach ($ra in $resultsjsonpolicyid)
     $res.nodeId               = $ra.nodeId
     $res.policyTestId         = $ra.policyTestId
     $results_final           += $res
-}
-
-$timespan                    = (New-TimeSpan -Start $start_time -End ((Get-Date).ToUniversalTime())).TotalMinutes
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "End script after $timespan minutes"
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-SyslogArr $syslog_Array
 }
 
 $results_final
