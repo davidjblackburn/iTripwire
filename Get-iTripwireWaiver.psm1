3b8d202cbc57bfd4dd2f9fe0e7c6ad0c5cd19022
davidjblackburn <40555710+davidjblackburn@users.noreply.github.com>
2019-12-28 21:15:19 -0800

    Update Get-iTripwireWaiver.psm1

--- Get-iTripwireWaiver.psm1
+++ Get-iTripwireWaiver.psm1
@@ -5,69 +5,17 @@ param (
     [string]$policyId,
     [string]$policyTestId,
     [string]$nodeId,
-    [string]$systemname               = "tripwire-prod.oa.caiso.com",
-    [string]$logserver                = "tripwire-prod.oa.caiso.com",
-    [string]$logdatabase              = "infosecrisks_prod",
-    [string]$logtable                 = "infosecrisksLog",
-    [switch]$logtoout                 = $false,
-    [switch]$logtoserver              = $false,
-    [int]$severity                    = 6
+    [string]$systemname               = "tripwire-prod.company.com"           # Use the Tripwire server name.
     )
-$start_time                  = Get-Date
 
 $headerplaintext             = @{"Accept"="text/plain"}
 $headerappjson               = @{"Accept"="application/json"}
 $results                     = $null
 
-$syslog_Array                = Set-SyslogArr
-
-$syslog_Array.facility       = 22
-$syslog_Array.severity       = $severity
-$syslog_Array.version        = 1
-$syslog_Array.hostname       = ([System.Net.DNS]::GetHostByName('').HostName).ToLower()
-$syslog_Array.appname        = ($MyInvocation.MyCommand).Name
-$syslog_Array.procid         = "-"
-$syslog_Array.msgid          = "calc"
-$syslog_Array.structureddata = "-"
-$syslog_Array.logdatabase    = $logdatabase
-$syslog_Array.logserver      = $logserver
-$syslog_Array.logtable       = $logtable
-$syslog_Array.logtoout       = $logtoout
-$syslog_Array.logtoserver    = $logtoserver
-
-
-if ( $severity -lt 0 -or $severity -gt 7 )
-{
-    $syslog_Array.msg        = "-LogLevel must be a number in the range [0..7]. Quitting collection."
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-
-    Write-SyslogArr $syslog_Array
-    return
-}
-
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "Start script"
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-SyslogArr $syslog_Array
-}
-
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "Get complete list of waivers"
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-SyslogArr $syslog_Array
-}
-
+# Get complete list of waivers
 $waiverlist                  = Get-iTripwirePolicyTestWaivers -websession $websession
 
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "Find PolicyId for test to check for waiver"
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-SyslogArr $syslog_Array
-}
-
+# Find PolicyId for test to check for waiver
 $policyresults               = $waiverlist | Where-Object { $_.policyId -eq $policyId }
 
 if ($policyresults.count -gt 0 -or $policyresults.id.Length -gt 0 )
@@ -76,13 +24,5 @@ if ($policyresults.count -gt 0 -or $policyresults.id.Length -gt 0 )
 }
 
 $results
-
-$timespan                    = (New-TimeSpan -Start $start_time -End (Get-Date)).TotalMinutes
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "End script after $timespan minutes"
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-SyslogArr $syslog_Array
-}
 
 }
