61f7fe7370fe83ced95f5ac6a4d137da81c43cbd
davidjblackburn <40555710+davidjblackburn@users.noreply.github.com>
2019-12-28 18:56:57 -0800

    Update Get-iTripwireElementsListNodeandRule.psm1

--- Get-iTripwireElementsListNodeandRule.psm1
+++ Get-iTripwireElementsListNodeandRule.psm1
@@ -2,46 +2,11 @@
 {
 param (
     $websession,
-    [string]$ruleid      = "-1y2p0ij32e7pq:-1y2p0ij30b3m3",
-    [string]$nodeid      = "-1y2p0ij32e8bv:-1y2p0ij30b7jb",
-    [string]$systemname  = "tripwire-prod.oa.caiso.com",
-    [string]$logserver   = "tripwire-prod.oa.caiso.com",
-    [int]$pagelimit      = 100,
-    [string]$logdatabase = "infosecrisks_prod",
-    [string]$logtable    = "InfoSecRisksLog",
-    [switch]$logtoout    = $true,
-    [switch]$logtoserver = $false,
-    [int]$severity       = 6
+    [string]$ruleid      = "-1y2p0lj36e7m7:-1y2p0ij42wmy7",  # Use a ruleId for testing.
+    [string]$nodeid      = "-1y2p0ij39e2bv:-1y2p0ij29b9jb",  # Use a nodeId for testing.
+    [string]$systemname  = "tripwire-prod.company.com",
+    [int]$pagelimit      = 100
     )
-$syslog_Array                = Set-SyslogArr
-
-$headerplaintext             = @{"Accept"="text/plain"}
-$headerappjson               = @{"Accept"="application/json"}
-
-$syslog_Array.facility       = 22
-$syslog_Array.severity       = $severity
-$syslog_Array.version        = 1
-$syslog_Array.hostname       = ([System.Net.DNS]::GetHostByName('').HostName).ToLower()
-$syslog_Array.appname        = ($MyInvocation.MyCommand).Name
-$syslog_Array.procid         = "-"
-$syslog_Array.msgid          = "calc"
-$syslog_Array.structureddata = "-"
-$syslog_Array.logdatabase    = $logdatabase
-$syslog_Array.logserver      = $logserver
-$syslog_Array.logtable       = $logtable
-$syslog_Array.logtoout       = $logtoout
-$syslog_Array.logtoserver    = $logtoserver
-
-
-if ( $severity -lt 0 -or $severity -gt 7 )
-{
-    $syslog_Array.msg        = "-LogLevel must be a number in the range [0..7]. Quitting collection."
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-
-    Write-SyslogArr $syslog_Array
-    return
-}
-
 add-type @"
     using System.Net;
     using System.Security.Cryptography.X509Certificates;
