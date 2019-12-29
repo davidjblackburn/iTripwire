01a667079587c21b1e8355cdfe852de1d416bc2a
davidjblackburn <40555710+davidjblackburn@users.noreply.github.com>
2019-12-28 21:15:11 -0800

    Update Get-iTripwireVersionElementContent.psm1

--- Get-iTripwireVersionElementContent.psm1
+++ Get-iTripwireVersionElementContent.psm1
@@ -2,43 +2,10 @@
 {
 param (
     $websession,
-    [string]$elementid   = "-1y2p0ij32e8ch:-1y2p0iixjime6",
-    [string]$systemname  = "tripwire-prod.oa.caiso.com",
-    [string]$logserver   = "tripwire-prod.oa.caiso.com",
-    [string]$logdatabase = "infosecrisks_prod",
-    [string]$logtable    = "InfoSecRisksLog",
-    [switch]$logtoout    = $false,
-    [switch]$logtoserver = $false,
-    [int]$severity       = 6
+    [string]$elementid   = "-1y2p0ij32e8ch:-1y2p0iixjime6",      # Use a elemendId for testing.
+    [string]$systemname  = "tripwire-prod.company.com"           # Use the Tripwire server name.
+
     )
-$syslog_Array                = Set-SyslogArr
-
-$headerplaintext             = @{"Accept"="text/plain"}
-$headerappjson               = @{"Accept"="application/json"}
-
-$syslog_Array.facility       = 22
-$syslog_Array.severity       = $severity
-$syslog_Array.version        = 1
-$syslog_Array.hostname       = ([System.Net.DNS]::GetHostByName('').HostName).ToLower()
-$syslog_Array.appname        = ($MyInvocation.MyCommand).Name
-$syslog_Array.procid         = "-"
-$syslog_Array.msgid          = "calc"
-$syslog_Array.structureddata = "-"
-$syslog_Array.logdatabase    = $logdatabase
-$syslog_Array.logserver      = $logserver
-$syslog_Array.logtable       = $logtable
-$syslog_Array.logtoout       = $logtoout
-$syslog_Array.logtoserver    = $logtoserver
-
-
-if ( $severity -lt 0 -or $severity -gt 7 )
-{
-    $syslog_Array.msg        = "-LogLevel must be a number in the range [0..7]. Quitting collection."
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-
-    Write-SyslogArr $syslog_Array
-    return
-}
 
 add-type @"
     using System.Net;
