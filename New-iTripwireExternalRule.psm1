b93f2d563e0679f388f0514e87e42357b3a48fe7
davidjblackburn <40555710+davidjblackburn@users.noreply.github.com>
2019-12-28 21:15:27 -0800

    Update New-iTripwireExternalRule.psm1

--- New-iTripwireExternalRule.psm1
+++ New-iTripwireExternalRule.psm1
@@ -2,44 +2,12 @@
 {
 param (
     $websession,
-    [string]$systemname     = "tripwire-prod.oa.caiso.com",
-    [string]$logserver      = "tripwire-prod.oa.caiso.com",
+    [string]$systemname     = "tripwire-prod.company.com",        # Use the Tripwire server name.
     [string]$rulename       = "rule",
-    [string]$ruledescription= "New rule",
-    [string]$logdatabase    = "infosecrisks_prod",
-    [string]$logtable       = "InfoSecRisksLog",
-    [switch]$logtoout       = $true,
-    [switch]$logtoserver    = $false,
-    [int]$severity          = 6
-
+    [string]$ruledescription= "New rule"
     )
-$syslog_Array                = Set-SyslogArr
 $headerplaintext             = @{"Accept"="text/plain"}
 $headerappjson               = @{"Accept"="application/json";"Content-Type"="application/json"}
-
-$syslog_Array.facility       = 22
-$syslog_Array.severity       = $severity
-$syslog_Array.version        = 1
-$syslog_Array.hostname       = ([System.Net.DNS]::GetHostByName('').HostName).ToLower()
-$syslog_Array.appname        = ($MyInvocation.MyCommand).Name
-$syslog_Array.procid         = "-"
-$syslog_Array.msgid          = "calc"
-$syslog_Array.structureddata = "-"
-$syslog_Array.logdatabase    = $logdatabase
-$syslog_Array.logserver      = $logserver
-$syslog_Array.logtable       = $logtable
-$syslog_Array.logtoout       = $logtoout
-$syslog_Array.logtoserver    = $logtoserver
-
-
-if ( $severity -lt 0 -or $severity -gt 7 )
-{
-    $syslog_Array.msg        = "-LogLevel must be a number in the range [0..7]. Quitting collection."
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-
-    Write-SyslogArr $syslog_Array
-    return
-}
 
 add-type @"
     using System.Net;
@@ -54,14 +22,6 @@ add-type @"
 "@
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-if ( $severity -ge 6 )
-{
-    $syslog_Array.msg        = "Start script."
-    $syslog_Array.timestamp  = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-
-    Write-SyslogArr $syslog_Array
-}
 
 $body = "{""name"":""$rulename"",""description"":""$ruledescription"",""type"":""External Rule""}"
 
