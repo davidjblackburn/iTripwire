49f46b8aa6e6b88905b25c7246e297a17a5b9a91
davidjblackburn <40555710+davidjblackburn@users.noreply.github.com>
2019-12-30 21:21:11 -0800

    Create Get-iTripwireWaiver.psm1

---
+++ modules/iTripwire/Get-iTripwireWaiver.psm1
@@ -0,0 +1,28 @@
+﻿Function Get-iTripwireWaiver
+{
+param (
+    $websession,
+    [string]$policyId,
+    [string]$policyTestId,
+    [string]$nodeId,
+    [string]$systemname               = "tripwire-prod.company.com"           # Use the Tripwire server name.
+    )
+
+$headerplaintext             = @{"Accept"="text/plain"}
+$headerappjson               = @{"Accept"="application/json"}
+$results                     = $null
+
+# Get complete list of waivers
+$waiverlist                  = Get-iTripwirePolicyTestWaivers -websession $websession
+
+# Find PolicyId for test to check for waiver
+$policyresults               = $waiverlist | Where-Object { $_.policyId -eq $policyId }
+
+if ($policyresults.count -gt 0 -or $policyresults.id.Length -gt 0 )
+{
+    $results                     = $policyresults | Where-Object { $_.waivedTests.nodeid -eq $nodeId -and $_.waivedTests.policytestid -eq $policyTestId } | Sort-Object -Property expiration -Descending | Select-Object -First 1
+}
+
+$results
+
+}
